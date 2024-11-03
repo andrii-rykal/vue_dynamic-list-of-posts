e11d85f978b51b9723ae4880ddbd740f8d605c6c
Andrii Rykal <andreuka79@gmail.com>
2024-11-03 19:27:09 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      55555555555555557777777555555555555555555555555555
+      5555555522555555555555555555555555555
       <HelloWorld msg="You did it!" />
     </div>
   </header>
